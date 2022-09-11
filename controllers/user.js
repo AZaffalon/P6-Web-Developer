99e948037fa90e0b3a6af51f68cc0a0990c5dab4
Alexis Zaffalon <67122137+AZaffalon@users.noreply.github.com>
2022-09-11 16:18:03 +0200

    Add email validation before creating the new user

--- controllers/user.js
+++ controllers/user.js
@@ -16,8 +16,13 @@ const iv = process.env.CRYPTOJS_IV;
 // Signup logic  
 exports.signup = (req, res, next) => {
   const cypherEmail = encrypt(req.body.email);
+
   bcrypt.hash(req.body.password, 10)
     .then(hash => {
+      // Test if email is valid
+      if (!validateEmail(req.body.email)) {
+        return res.status(404).json({ message: 'Invalid Email !'});
+      }
       const user = new User({
         email: cypherEmail,
         password: hash
@@ -36,28 +41,28 @@ exports.signup = (req, res, next) => {
 // Sign-in logic
 exports.login = (req, res, next) => {
   User.findOne({ email: encrypt(req.body.email) })
-  .then(user => {
-    if (!user) {
+    .then(user => {
+      if (!user) {
           return res.status(404).json({ message: 'User does not exist !'});
-      }
+        }
       bcrypt.compare(req.body.password, user.password)
-          .then(valid => {
-              if (!valid) {
-                  return res.status(401).json({ message: 'Invalid password !' });
-              }
-              res.status(200).json({
-                  userId: user._id,
-                  user: user,
-                  token: jwt.sign(
-                    { userId: user._id },
-                    `${process.env.JWT_TOKEN_SECRET}`,
-                    { expiresIn: '24h' } // l'utilisateur devra se reconnecter au bout de 24h
-                  )
-              });
-          })
-          .catch(error => res.status(500).json({ error }));
-  })
-  .catch(error => res.status(500).json({ error }));
+        .then(valid => {
+          if (!valid) {
+            return res.status(401).json({ message: 'Invalid password !' });
+          }
+          res.status(200).json({
+            userId: user._id,
+            user: user,
+            token: jwt.sign(
+              { userId: user._id },
+              `${process.env.JWT_TOKEN_SECRET}`,
+              { expiresIn: '24h' } // l'utilisateur devra se reconnecter au bout de 24h
+            )
+          });
+        })
+        .catch(error => res.status(500).json({ error }));
+    })
+    .catch(error => res.status(500).json({ error }));
 };
 
 // encrypt email using crypto-js AES
@@ -78,4 +83,10 @@ function decrypt(encrypted_string) {
     padding: CryptoJS.pad.Pkcs7
   });
   return bytes.toString(CryptoJS.enc.Utf8);
+}
+
+// Email validity
+function validateEmail(val) {
+  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(val);
 }
