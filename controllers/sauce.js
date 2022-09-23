b1fc25c3e33bcf67f4ef027264c84e1442297427
Alexis Zaffalon <67122137+AZaffalon@users.noreply.github.com>
2022-09-23 15:49:44 +0200

    Add POST route to create a sauce without multer yet

--- controllers/sauce.js
+++ controllers/sauce.js
@@ -12,3 +12,23 @@ exports.readOneSauce = (req, res, next) => {
     .then(sauce => res.status(200).json({sauce}))
     .catch(error => res.status(500).json({error}));
 };
+
+exports.createSauce = (req, res, next) => {
+  delete req.body._id;
+  const sauce = new Sauce({
+    userId: req.body.userId,
+    name: req.body.name,
+    manufacturer: req.body.manufacturer,
+    description: req.body.description,
+    mainPepper: req.body.mainPepper, 
+    imageUrl: req.body.imageUrl,
+    heat: req.body.heat,
+    likes: req.body.likes,
+    dislikes: req.body.dislikes,
+    usersLiked: req.body.usersLiked,
+    usersDisliked: req.body.usersDisliked
+  });
+  sauce.save()
+    .then(() => res.status(201).json({message: 'Sauce créée !'}))
+    .catch(error => res.status(401).json({error}));
+};
--- routes/sauce.js
+++ routes/sauce.js
@@ -5,5 +5,6 @@ const sauceController = require('../controllers/sauce');
 
 router.get('/sauces', sauceController.readAllSauces);
 router.get('/sauces/:id', sauceController.readOneSauce);
+router.post('/sauces', sauceController.createSauce);
 
 module.exports = router;
