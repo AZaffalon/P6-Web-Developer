b8caf165ffc5d38a4c8ddad49127e5d2f690287d
Alexis Zaffalon <67122137+AZaffalon@users.noreply.github.com>
2022-09-30 10:22:49 +0200

    Add routes post for likes/dislikes

--- routes/sauce.js
+++ routes/sauce.js
@@ -9,5 +9,7 @@ const sauceController = require('../controllers/sauce');
 router.get('/sauces', auth, sauceController.readAllSauces);
 router.get('/sauces/:id', auth, sauceController.readOneSauce);
 router.post('/sauces', auth, multer, sauceController.createSauce);
+router.post('sauces/:id/like', auth, sauceController.likeSauce);
+router.put('/sauces', auth, multer, sauceController.updateSauce);
 
 module.exports = router;
