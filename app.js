616b84da150d315d011e442e61f12f44e0b83cf4
Alexis Zaffalon <67122137+AZaffalon@users.noreply.github.com>
2022-10-16 12:17:10 +0200

    Update package.json for node 16.13.1 + add helmet middleware and allow cors only for frontend

--- app.js
+++ app.js
@@ -1,38 +1,23 @@
 const express = require('express');
+// Set various http headers to help secure the app
+const helmet = require("helmet");
 const cors = require('cors');
+/**
+ * Allow CORS access only for 1 client (Frontend)
+ */
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (process.env.WHITELIST.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  }
+};
+
 const mongoSanitize = require('express-mongo-sanitize');
 // Limit number of request per window in a certain time
 const rateLimit = require('express-rate-limit');
-// Limit spped of request after a certain number
-const slowDown = require("express-slow-down");
-const app = express();
-
-const path = require('path');
-
-const userRoutes = require('./routes/user');
-const sauceRoutes = require('./routes/sauce');
-
-require('./config/db.config'); // Connection to mongoDB
-
-
-app.use(cors());
-
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-  next();
-});
-
-app.use(express.json());
-app.use(express.urlencoded({
-  extended: true
-}));
-
-/**
- * Prevent SQL injection by sanatizing the received data
- */
-app.use(mongoSanitize());
 
 /**
  * Limiter number of requests for login and signup routes
@@ -54,6 +39,9 @@ const limiter = rateLimit({
   legacyHeaders: false, // Disable the `X-RateLimit-*` headers
 });
 
+// Limit spped of request after a certain number
+const slowDown = require("express-slow-down");
+
 /**
  * Limit speed of requests after a 100
  */
@@ -62,6 +50,29 @@ const speedLimiter = slowDown({
   delayAfter: 100, // allow 100 requests to go at full-speed, then...
   delayMs: 500 // 101th request has a 500ms delay, 7th has a 1000ms delay, 8th gets 1500ms, etc.
 });
+
+const app = express();
+
+const path = require('path');
+
+const userRoutes = require('./routes/user');
+const sauceRoutes = require('./routes/sauce');
+
+require('./config/db.config'); // Connection to mongoDB
+
+app.use(cors(corsOptions));
+
+app.use(helmet());
+
+app.use(express.json());
+app.use(express.urlencoded({
+  extended: true
+}));
+
+/**
+ * Prevent SQL injection by sanatizing the received data
+ */
+app.use(mongoSanitize());
 
 app.use('/images', express.static(path.join(__dirname, 'images')));
 app.use('/api/auth', authLimiter, userRoutes); // login & signup routes for User
