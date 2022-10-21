e974fd20c50b42abe2c025ae61a6defb263f7208
Alexis Zaffalon <67122137+AZaffalon@users.noreply.github.com>
2022-10-21 09:23:53 +0200

    Add unlinkSync image in the update method

--- controllers/sauce.js
+++ controllers/sauce.js
@@ -45,11 +45,15 @@ exports.updateSauce = (req, res, next) => {
   Sauce.findOne({_id: req.params.id})
     .then((sauce) => {
         if (sauce.userId != req.auth.userId) {
-            res.status(401).json({ message : 'Not authorized'});
+          res.status(401).json({ message : 'Not authorized'});
         } else {
-            Sauce.updateOne({ _id: req.params.id}, { ...sauceObject, _id: req.params.id})
-            .then(() => res.status(200).json({message : 'Sauce modified!'}))
-            .catch(error => res.status(401).json({ error }));
+          // Remove the old image from directory before adding the new one
+          const filename = sauce.imageUrl.split('/images/')[1];
+          fs.unlinkSync(`images/${filename}`);
+
+          Sauce.updateOne({ _id: req.params.id}, { ...sauceObject, _id: req.params.id})
+          .then(() => res.status(200).json({message : 'Sauce modified!'}))
+          .catch(error => res.status(401).json({ error }));
         }
     })
     .catch((error) => {
